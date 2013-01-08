aef72d61b2b6a64dc86ba501e2a9ae1353630306
softwareology <tboland@sd64.bc.ca>
2013-01-08 12:34:24 -0800

    Create dat.py

---
+++ dat.py
@@ -0,0 +1,148 @@
+import pygame
+from pygame import *
+
+zero = pygame.image.load("Data\\0.bmp")
+zerorect = zero.get_rect()
+zerorect.left = 7
+zerorect.top = 173
+
+zerob = pygame.image.load("Data\\0_2.bmp")
+
+one = pygame.image.load("Data\\1.bmp")
+onerect = one.get_rect()
+onerect.left = 7
+onerect.top = 139
+
+oneb = pygame.image.load("Data\\1_2.bmp")
+
+two = pygame.image.load("Data\\2.bmp")
+tworect = two.get_rect()
+tworect.left = 48
+tworect.top = 139
+
+twob = pygame.image.load("Data\\2_2.bmp")
+
+three = pygame.image.load("Data\\3.bmp")
+threerect = three.get_rect()
+threerect.left = 89
+threerect.top = 139
+
+threeb = pygame.image.load("Data\\3_2.bmp")
+
+four = pygame.image.load("Data\\4.bmp")
+fourrect = four.get_rect()
+fourrect.left = 7
+fourrect.top = 105
+
+fourb = pygame.image.load("Data\\4_2.bmp")
+
+five = pygame.image.load("Data\\5.bmp")
+fiverect = five.get_rect()
+fiverect.left = 48
+fiverect.top = 105
+
+fiveb = pygame.image.load("Data\\5_2.bmp")
+
+six = pygame.image.load("Data\\6.bmp")
+sixrect = six.get_rect()
+sixrect.left = 89
+sixrect.top = 105
+
+sixb = pygame.image.load("Data\\6_2.bmp")
+
+seven = pygame.image.load("Data\\7.bmp")
+sevenrect = seven.get_rect()
+sevenrect.left = 7
+sevenrect.top = 71
+
+sevenb = pygame.image.load("Data\\7_2.bmp")
+
+eight = pygame.image.load("Data\\8.bmp")
+eightrect = eight.get_rect()
+eightrect.left = 48
+eightrect.top = 71
+
+eightb = pygame.image.load("Data\\8_2.bmp")
+
+nine = pygame.image.load("Data\\9.bmp")
+ninerect = nine.get_rect()
+ninerect.left = 89
+ninerect.top = 71
+
+nineb = pygame.image.load("Data\\9_2.bmp")
+
+add = pygame.image.load("Data\\Add.bmp")
+addrect = add.get_rect()
+addrect.left = 130
+addrect.top = 173
+
+addb = pygame.image.load("Data\\Add2.bmp")
+
+sub = pygame.image.load("Data\\Subtract.bmp")
+subrect = sub.get_rect()
+subrect.left = 130
+subrect.top = 139
+
+subb = pygame.image.load("Data\\Subtract2.bmp")
+
+mult = pygame.image.load("Data\\Multiply.bmp")
+multrect = mult.get_rect()
+multrect.left = 130
+multrect.top = 105
+
+multb = pygame.image.load("Data\\Multiply2.bmp")
+
+div = pygame.image.load("Data\\Divide.bmp")
+divrect = div.get_rect()
+divrect.left = 130
+divrect.top = 71
+
+divb = pygame.image.load("Data\\Divide2.bmp")
+
+fract = pygame.image.load("Data\\Over.bmp")
+fractrect = fract.get_rect()
+fractrect.left = 171
+fractrect.top = 71
+
+fractb = pygame.image.load("Data\\Over2.bmp")
+
+mix = pygame.image.load("Data\\Mixed.bmp")
+mixrect = mix.get_rect()
+mixrect.left = 171
+mixrect.top = 105
+
+mixb = pygame.image.load("Data\\Mixed2.bmp")
+
+ce = pygame.image.load("Data\\CE.bmp")
+cerect = ce.get_rect()
+cerect.left = 77
+cerect.top = 37
+
+ceb = pygame.image.load("Data\\CE2.bmp")
+
+back = pygame.image.load("Data\\Backspace.bmp")
+backrect = back.get_rect()
+backrect.left = 7
+backrect.top = 37
+
+backb = pygame.image.load("Data\\Backspace2.bmp")
+
+eq = pygame.image.load("Data\\Equal.bmp")
+eqrect = eq.get_rect()
+eqrect.left = 171
+eqrect.top = 139
+
+eqb = pygame.image.load("Data\\Equal2.bmp")
+
+copy = pygame.image.load("Data\\Copy.bmp")
+copyrect = copy.get_rect()
+copyrect.left = 48
+copyrect.top = 173
+
+copyb = pygame.image.load("Data\\Copy2.bmp")
+
+box = pygame.image.load("Data\\Textbox.bmp")
+boxrect = box.get_rect()
+boxrect.left = 7
+boxrect.top = 7
+
